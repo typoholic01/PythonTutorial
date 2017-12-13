d3c593f28e5a0be2168b70699ba3d72ab3f6fb63
HOME\typoholic01 <typoholic01@gmail.com>
2017-12-14 00:26:53 +0900

    기본문법

--- index.py
+++ index.py
@@ -1,9 +1,31 @@
 """
-@author: moon497@edkim.co
+@author: typoholic01@gmail.com
 """
 
+# 상수선언
 A = 1
+
+# if문
 if A > 0:
     print('Hello Python!')
 else:
     print('Hell Python!')
+
+# for문
+for a in [1, 2, 3]:
+    print(a)
+
+# while문
+B = 0
+while B < 3:
+    B = B + 1
+    print(B)
+
+# 함수
+def sum2(i, j):
+    """
+    @author: typoholic01@gmail.com
+    """
+    return i + j
+
+print(sum(41, 2))
